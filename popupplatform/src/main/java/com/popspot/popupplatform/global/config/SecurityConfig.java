35ca22fa5130c9d2fbf6994908624eb5b11319ca
JaeGeon-SMU <dlworjs1021@gmail.com>
2025-11-23 11:14:16 +0900

    fix : swaager-ui security에 막히던 부분 수정

--- popupplatform/src/main/java/com/popspot/popupplatform/global/config/SecurityConfig.java
+++ popupplatform/src/main/java/com/popspot/popupplatform/global/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger"
                         ).permitAll()
                         .requestMatchers("/api/auth/phone/**",
                                 "/api/auth/**",
