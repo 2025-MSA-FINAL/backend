95ad94d72a9457d5f6f93819e2e2fb919e5ee4e7
HeeJeongSeo <heejung9965@naver.com>
2025-12-05 17:41:23 +0900

    feat: 검색 기능 추가

--- popupplatform/src/main/java/com/popspot/popupplatform/controller/chat/ChatPopupController.java
+++ popupplatform/src/main/java/com/popspot/popupplatform/controller/chat/ChatPopupController.java
@@ -22,7 +22,9 @@ public class ChatPopupController {
             summary = "전체 팝업 리스트 조회",
             description = "채팅에서 사용할 팝업 이름 목록을 최신순(created_at DESC)으로 조회합니다."
     )
-    public ResponseEntity<PopupSimpleListResponse> getPopupList() {
-        return ResponseEntity.ok(chatPopupService.getPopupList());
+    public ResponseEntity<PopupSimpleListResponse> getPopupList(
+            @RequestParam(required = false) String keyword
+    ) {
+        return ResponseEntity.ok(chatPopupService.getPopupList(keyword));
     }
 }
--- popupplatform/src/main/java/com/popspot/popupplatform/service/chat/ChatPopupService.java
+++ popupplatform/src/main/java/com/popspot/popupplatform/service/chat/ChatPopupService.java
@@ -12,9 +12,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatPopupService {
     private final ChatPopupMapper chatPopupMapper;
-    public PopupSimpleListResponse getPopupList() {
+    public PopupSimpleListResponse getPopupList(String keyword) {
         //모든 팝업이름 목록으로 가져와서 list 저장
-        List<PopupSimpleResponse> list = chatPopupMapper.findAllPopupNames();
+        List<PopupSimpleResponse> list;
+
+        if (keyword != null && !keyword.isBlank()) {
+            list = chatPopupMapper.searchPopupNames(keyword);
+        } else {
+            list = chatPopupMapper.findAllPopupNames();
+        }
+
         return PopupSimpleListResponse.builder()
                 .count(list.size()) //list 개수
                 .popups(list)
