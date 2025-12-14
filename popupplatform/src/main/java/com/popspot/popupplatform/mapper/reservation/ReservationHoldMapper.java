2991b2c3bc9c4469fe619ca6cbed9943551a12bd
JaeGeon-SMU <dlworjs1021@gmail.com>
2025-12-14 12:59:38 +0900

    refactor : 불필요한 코드 삭제

--- popupplatform/src/main/java/com/popspot/popupplatform/mapper/reservation/ReservationHoldMapper.java
+++ popupplatform/src/main/java/com/popspot/popupplatform/mapper/reservation/ReservationHoldMapper.java
@@ -2,10 +2,7 @@ package com.popspot.popupplatform.mapper.reservation;
 
 import com.popspot.popupplatform.domain.reservation.ReservationHold;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Mapper
 public interface ReservationHoldMapper {
