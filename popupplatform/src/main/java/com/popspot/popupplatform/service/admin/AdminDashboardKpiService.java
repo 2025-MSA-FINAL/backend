8867fa54156e594c33d3831b533edbc0b1f7a522
ChangHayeong <bestdev.hy@gmail.com>
2025-12-22 19:16:06 +0900

    feat: add admin dashboard KPI stats API and service, change data loading to real-time

---
+++ popupplatform/src/main/java/com/popspot/popupplatform/service/admin/AdminDashboardKpiService.java
@@ -0,0 +1,7 @@
+package com.popspot.popupplatform.service.admin;
+
+import com.popspot.popupplatform.dto.admin.KpiStatsDTO;
+
+public interface AdminDashboardKpiService {
+    KpiStatsDTO getKpiStats();
+}
